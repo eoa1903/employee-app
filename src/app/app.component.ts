1b54525ae6edee9a8e28d797c83f1d929f6d7f9c
Ekundayo Alabi <ekundayoa@plasmacomp.com>
2022-07-15 14:34:23 -0500

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,18 @@ import { EmployeeServiceService } from './service/employee-service.service';
 export class AppComponent implements OnInit {
   title = 'employee-app';
   employees: Employee[] = [];
+  data:any=[]
+
 
   constructor(private employeeService: EmployeeServiceService){}
 
   ngOnInit(): void {
-      this.employeeService.findAll().subscribe( data =>{
-        this.employees = data});
+      // this.employeeService.findAll().subscribe( data =>{
+      //   this.employees = data});
+
+        this.employeeService.listAll().subscribe((data: { [x: string]: any; }) => {
+          this.data = Array.from(Object.keys(data), k =>data[k]); 
+        });
   }
 
 }
--- src/app/service/employee-service.service.ts
+++ src/app/service/employee-service.service.ts
@@ -7,12 +7,22 @@ import { Employee } from '../entity/Employee';
   providedIn: 'root'
 })
 export class EmployeeServiceService {
-  private url='http://localhost:8080/all';
+  public employe: any;
+  private url='http://localhost:8080/employee';
 
   constructor(private http: HttpClient) { 
   }
 
-  public findAll(): Observable<Employee[]> {
-    return this.http.get<Employee[]>(this.url);
+  // public findAll(): Observable<Employee[]> {
+  //   this.employe = this.http.get<Employee[]>(this.url);
+  //   this.employe.subscribe((data: any) =>{ console.log(data)})
+  //   return this.employe;
+  // }
+
+  public listAll(): any{
+    let timeNow = new Date();
+    let t = this.http.get(this.url);
+    let timeLater = new Date();
+    return t;
   }
 }
